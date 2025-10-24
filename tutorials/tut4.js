328b5aef4d1f923e50b5fe3c21e7149fc10d7348
I-Hua24 <iris_huang0129@hotmail.com>
2025-10-24 01:23:10 +0000

    week 6

--- tutorials/tut4.js
+++ tutorials/tut4.js
@@ -1,5 +1,20 @@
-let w=20
-let h=5
-let a=w*h
+quote="Click the button to generate a motivational quote!";
+document.getElementById("quote").innerHTML=quote;
+function newQuote(){
+    var randomNumber = Math.floor(Math.random()*(quotes.length));
+    document.getElementById("quote").innerHTML=quotes[randomNumber];
+}   
 
-document.write("<div><h1>Rectangle Area Calculator</h1> <p>the rectangle of width "+w+"and height"+h+"area is"+a+"</p></div>")
+
+const quotes = [
+  "Believe you can and you're halfway there.",
+  "Push yourself, because no one else is going to do it for you.",
+  "Success is what comes after you stop making excuses.",
+  "The harder you work for something, the greater you’ll feel when you achieve it.",
+  "Dream it. Wish it. Do it.",
+  "Don’t stop when you’re tired. Stop when you’re done.",
+  "Great things never come from comfort zones.",
+  "Wake up with determination. Go to bed with satisfaction.",
+  "It always seems impossible until it’s done.",
+  "Discipline is the bridge between goals and accomplishment."
+];
